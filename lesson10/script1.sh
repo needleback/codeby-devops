96a23d6d031bae34dbb2ce8e9a7bb898ce8a91a3
needleback <gotham86@bk.ru>
2026-05-18 03:15:06 -0400

    Homework lesson10
    Homework lesson10

---
+++ lesson10/script1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+MYFOLDER=/home/${USER}/myfolder
+
+if [ -d ${MYFOLDER} ]; then
+  echo "The directory ${MYFOLDER} already exists, Deleting it"
+  rm -r ${MYFOLDER}/
+fi
+
+# Создает папку myfolder в домашней папке текущего пользователя
+mkdir ${MYFOLDER}/
+
+# имеет две строки: 1) приветствие, 2) текущее время и дата
+touch ${MYFOLDER}/file1
+echo 'Hello world' > ${MYFOLDER}/file1
+echo $(date) >> ${MYFOLDER}/file1
+
+# пустой файл с правами 777
+touch ${MYFOLDER}/file2
+chmod 777 ${MYFOLDER}/file2
+
+# одна строка длиной в 20 случайных символов
+touch ${MYFOLDER}/file3
+echo $( head -c 30 /dev/urandom | base64 | tr -dc 'A-Za-z' | head -c 20; echo ) > ${MYFOLDER}/file3
+
+# пустые файлы
+touch ${MYFOLDER}/file{4..5}
+
+echo "${0} completed successfully"
